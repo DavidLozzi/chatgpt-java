0f378e5b4bcfb4c10f4e99415cba270cd198fad5
David Lozzi <david@lozzi.net>
2023-09-09 14:07:55 -0400

    initial

---
+++ src/main/java/com/davidlozzi/chatgpt/ChatgptApplication.java
@@ -0,0 +1,24 @@
+package com.davidlozzi.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ChatgptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChatgptApplication.class, args);
+  }
+
+  @GetMapping("/chat")
+  @ResponseBody
+  public ResponseEntity chat() {
+    return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+  }
+}
